f8806e0ae3f48d1346f673d08bb20327088df242
sowunmi mayowa <smayowa176@gmail.com>
2024-04-05 19:16:53 +0100

    added styles and fixed signin with google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,28 +33,41 @@ const Login = () => {
         reset()
     }
   return (
-    <div>
-        <h1>LOgin here</h1>
-            <form onSubmit={handleSubmit(onSubmitHandler)}> 
-                <div>
-                    <label htmlFor="email">email</label>
-                    <input type="email" name="email" id="email" {...register("email")} />
-                    <p>{errors.email?.message}</p>
+    <div className="mt-8">
+        <div className="flex justify-center items-center w-4/5 md:w-1/2 mx-auto">
+            <form className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl" onSubmit={handleSubmit(onSubmitHandler)}>
+                <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Sign In</h3>
+                <div  className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300 cursor-pointer">
+                    <img className="h-5 mr-2" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png" alt="google" />
+                    Sign in with Google
                 </div>
-                <div>
-                    <label htmlFor="password">password</label>
-                    <input type="password" name="password" id="password" {...register("password")} />
-                    <p>{errors.password?.message}</p>
+                <div className="flex items-center mb-3">
+                    <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                    <p className="mx-4 text-grey-600">or</p>
+                    <hr className="h-0 border-b border-solid border-grey-500 grow" />
                 </div>
-                <div>
-                <input type="submit" value="Login" onClick={handleSubmit(onSubmitHandler)} />
-            </div>
+                
+                <div className="flex flex-col flex-start">
+                    <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">Email*</label>
+                    <input id="email" type="email" placeholder="mail@loopple.com" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl bg-gray-200" {...register("email")}/>
+                    <p className="text-red-800 text-xs">{errors.email?.message}</p>
+                </div>
+                <div className="flex flex-col flex-start">
+                    <label htmlFor="password" className="mb-2 text-sm text-start text-grey-900">Password*</label>
+                    <input id="password" type="password" placeholder="Enter a password" className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl bg-gray-200" {...register("password")} />
+                    <p className="text-red-800 text-xs">{errors.password?.message}</p>
+                </div>
+                <div className="flex flex-row justify-between mb-8">
+                    <label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
+                    </label>
+                    <a className="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
+                </div>
+                <button className="bg-blue-600 h-12 rounded-lg text-white text-lg font-semibold" onClick={handleSubmit(onSubmitHandler)}>Sign In</button>
+                <p className="text-sm leading-relaxed text-grey-900">Dont have an account? <Link to="/register"  className="font-bold text-grey-700">Sign up</Link></p>
             </form>
-        <div>
-            <p>Don't have an account? <Link to="/register">register</Link></p>
         </div>
     </div>
-  )
+)
 }
 
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { auth, provider } from "../config/config";
-import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
@@ -21,68 +21,78 @@ const Register = () => {
 
     const onSubmitHandler = (data) => {
         createUserWithEmailAndPassword(auth, data.email, data.password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user)
-        })
-        .catch(error => {
-            console.log(error.message)
-        })
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user)
+                navigate("/home")
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
         reset()
     }
 
     //sign in with google
     const signInWithGoogle = () => {
         signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user)
-            navigate("/home")
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ...
-        });
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user)
+                navigate("/home")
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage)
+                // ...
+            });
     }
-  return (
-    <div>
-        <h1>register here</h1>
-        <form onSubmit={handleSubmit(onSubmitHandler)}>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" {...register("username")} />
-                <p>{errors.username?.message}</p>
+    return (
+        <div className="mt-8">
+            <div className="flex justify-center items-center w-4/5 md:w-1/2 mx-auto">
+                <form className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl" onSubmit={handleSubmit(onSubmitHandler)}>
+                    <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Sign Up</h3>
+                    <div  className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300 cursor-pointer" onClick={signInWithGoogle}>
+                        <img className="h-5 mr-2" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png" alt="google" />
+                        Sign in with Google
+                    </div>
+                    <div className="flex items-center mb-3">
+                        <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                        <p className="mx-4 text-grey-600">or</p>
+                        <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                    </div>
+                    <div className="flex flex-col flex-start">
+                        <label htmlFor="username" className="mb-2 text-sm text-start text-grey-900">Username*</label>
+                        <input id="email" type="email" placeholder="mail@loopple.com" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 bg-gray-200 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" {...register("username")} />
+                        <p className="text-red-800 text-xs">{errors.username?.message}</p>
+                    </div>
+                    <div className="flex flex-col flex-start">
+                        <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">Email*</label>
+                        <input id="email" type="email" placeholder="mail@loopple.com" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl bg-gray-200" {...register("email")}/>
+                        <p className="text-red-800 text-xs">{errors.email?.message}</p>
+                    </div>
+                    <div className="flex flex-col flex-start">
+                        <label htmlFor="password" className="mb-2 text-sm text-start text-grey-900">Password*</label>
+                        <input id="password" type="password" placeholder="Enter a password" className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl bg-gray-200" {...register("password")} />
+                        <p className="text-red-800 text-xs">{errors.password?.message}</p>
+                    </div>
+                    <div className="flex flex-row justify-between mb-8">
+                        <label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
+                        </label>
+                        <a className="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
+                    </div>
+                    <button className="bg-blue-600 h-12 rounded-lg text-white text-lg font-semibold" onClick={handleSubmit(onSubmitHandler)}>Sign In</button>
+                    <p className="text-sm leading-relaxed text-grey-900">Already have an account? <Link to="/login"  className="font-bold text-grey-700">Sign up</Link></p>
+                </form>
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" {...register("email")} />
-                <p>{errors.email?.message}</p>
-            </div>
-            
-            <div>
-                <label htmlFor="password">password</label>
-                <input type="password" name="password" id="password" {...register("password")} />
-                <p>{errors.password?.message}</p>
-            </div>
-            <div>
-                <input type="submit" value="Sign up" onClick={handleSubmit(onSubmitHandler)}/>
-            </div>
-        </form>
-        <div>
-            <p>Do you have an account? <Link to="/login">login</Link></p>
         </div>
-        <div>
-            <p  className="cursor-pointer" onClick={signInWithGoogle}>Sign up with google</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Register
