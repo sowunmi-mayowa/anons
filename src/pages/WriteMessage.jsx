974779d4570f5017e2b0e517cef6c8a5cd21a150
sowunmi mayowa <smayowa176@gmail.com>
2024-04-20 22:43:50 +0100

    updates

--- src/pages/WriteMessage.jsx
+++ src/pages/WriteMessage.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { auth, db } from "../config/config";
+import { useParams } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
 
 const WriteMessage = () => {
+
+  const { id } = useParams()
+
+  const schema = yup.object().shape({
+    message: yup.string().required(),
+})
+const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+});
+
+const onSubmitHandler = async (data) => {
+  try{
+    const docref = await addDoc(collection(db, "messages"), {
+      message: data.message,
+      uid: id
+    })
+    alert("message sent")
+  }catch(error){
+    console.log(error.message )
+  }
+  reset();
+}
   return (
-    <div>WriteMessage</div>
+    <div>
+      <h1>Send message</h1>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+         <div>
+          <p> {errors.message?.message} </p>
+            <textarea name="message" id="message" cols="30" rows="10" className='border-2 border-black' {...register("message")}></textarea> <br />
+            <input type="submit"  className="bg-blue-500 text-black font-bold text-lg capitalize px-6 py-2 cursor-pointer" onClick={handleSubmit(onSubmitHandler)} />
+         </div>
+      </form>
+    </div>
   )
 }
 
