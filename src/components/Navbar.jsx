6ef82875743e4ab872f6da11c343c0ab88ab81c1
sowunmi mayowa <smayowa176@gmail.com>
2024-06-08 07:58:14 +0100

    added styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import anonLogo from "../assets/anonlogo.png"
+import home from "../assets/home.png"
+import message from "../assets/message.png"
+import avatar from "../assets/avatar.png"
 
 const Navbar = () => {
   return (
     <div>
-      <div className='flex justify-between bg-blue-700 text-white p-6'>
-        <div className='w-32'>
-          <img src={anonLogo} alt="logo" />
-        </div>
-        <div>
-          <button>sign out</button>
+      <div style={{
+        backgroundImage: "linear-gradient(104deg, #DFD6F6, #BECBF7)",
+        position: "fixed",
+        bottom: 0,
+        right: 0
+      }} className='flex justify-between text-white p-6 w-full h-20 rounded-t-3xl z-10'>
+        <div className='flex justify-between items-center w-full px-8'>
+          <img src={home} alt="icons" className='w- full h-full' />
+          <img src={message} alt="icons" className='w- full h-full' />
+          <img src={avatar} alt="icons" className='w- full h-full' />
         </div>
       </div>
     </div>
