e562d48e48457d2b2032e67d389d8ebf8993ac0c
sowunmi mayowa <smayowa176@gmail.com>
2024-06-14 22:13:49 +0100

    changed icons width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
         right: 0
       }} className='flex justify-between text-white p-6 w-full h-20 rounded-t-3xl z-10'>
         <div className='flex justify-between items-center w-full px-8'>
-          <Link to='/home' className='w-8'>
+          <Link to='/home' className='max-w-[30px]'>
             <img src={home} alt="icons" className='w- full h-full' />
           </Link>
-          <Link to='/messages' className='w-8'>
+          <Link to='/messages' className='max-w-[30px]'>
             <img src={message} alt="icons" className='w- full h-full' />
           </Link>
-          <Link to='/profile' className='w-8'>
+          <Link to='/profile' className='max-w-[30px]'>
             <img src={avatar} alt="icons" className='w- full h-full' />
           </Link>
         </div>
