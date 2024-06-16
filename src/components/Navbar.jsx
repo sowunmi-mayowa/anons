e75ae0d00651bb10861db6bdabf6f39e47fab4b7
sowunmi mayowa <smayowa176@gmail.com>
2024-06-16 20:02:50 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,17 +11,18 @@ const Navbar = () => {
         backgroundImage: "linear-gradient(104deg, #DFD6F6, #BECBF7)",
         position: "fixed",
         bottom: 0,
-        right: 0
+        right: 0,
+        left: 0 // Make sure the navbar spans the full width
       }} className='flex justify-between text-white p-6 w-full h-20 rounded-t-3xl z-10'>
-        <div className='flex justify-between items-center w-full px-8'>
+        <div className='flex justify-between items-center w-full max-w-screen-lg mx-auto px-8'>
           <Link to='/home' className='max-w-[30px] block'>
-            <img src={home} alt="icons" className='w- full h-full' />
+            <img src={home} alt="icons" className='w-full h-full' />
           </Link>
           <Link to='/messages' className='max-w-[30px] block'>
-            <img src={message} alt="icons" className='w- full h-full' />
+            <img src={message} alt="icons" className='w-full h-full' />
           </Link>
           <Link to='/profile' className='max-w-[30px] block'>
-            <img src={avatar} alt="icons" className='w- full h-full' />
+            <img src={avatar} alt="icons" className='w-full h-full' />
           </Link>
         </div>
       </div>
