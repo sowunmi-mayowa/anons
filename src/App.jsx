a74a578dc7fb2e86d725ff70df351348e9b90f7c
sowunmi mayowa <smayowa176@gmail.com>
2024-06-18 09:19:06 +0100

    added error element

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,19 @@ import Home from "./pages/Home"
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import Profile from "./pages/Profile";
+import Error from './components/Error'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/register" element={< Register />} />
-        <Route path="/login" element={< Login />} />
-        <Route path="/messages" element={< Messages />} />
-        <Route path="/:id" element={< WriteMessage />} />
-        <Route path="/home" element={< Home />} />
-        <Route path="/profile" element={< Profile />} />
+        <Route path="/" element={<Welcome />} errorElement={< Error/>} />
+        <Route path="/register" element={< Register />} errorElement={< Error/>} />
+        <Route path="/login" element={< Login />} errorElement={< Error/>} />
+        <Route path="/messages" element={< Messages />} errorElement={< Error/>} />
+        <Route path="/:id" element={< WriteMessage />} errorElement={< Error/>} />
+        <Route path="/home" element={< Home />} errorElement={< Error/>} />
+        <Route path="/profile" element={< Profile />} errorElement={< Error/>} />
 
       </>
     )
