e96b276fac917f43d4e12dd4714630ee5f893fa3
sowunmi mayowa <smayowa176@gmail.com>
2024-08-21 09:15:36 +0100

    fixed reload error

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,20 +1,27 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { auth } from "../config/config";
 import { onAuthStateChanged } from "firebase/auth";
 
 const ProtectedRoute = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
+      setLoading(false); // Set loading to false once the auth state is determined
     });
 
     return () => unsubscribe();
   }, []);
 
-  if (user === null) {
+  if (loading) {
+    // While checking the auth state, show a loading indicator or nothing
+    return <div>Loading...</div>; // You can replace this with a spinner or another UI element
+  }
+
+  if (!user) {
     // Redirect to the login page if the user is not authenticated
     return <Navigate to="/login" />;
   }
